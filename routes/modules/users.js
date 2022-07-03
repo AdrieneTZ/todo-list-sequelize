a79c5c8d0cda21ec552b1b3a0d5ba894ed4d9384
AdrieneTZ <tznali17@gmail.com>
2022-07-03 16:44:11 +0800

    feat: add function: logout

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -54,7 +54,9 @@ router.post('/login', passport.authenticate('local', {
 
 // router for logout
 router.get('/logout', (req, res) => {
-  res.send('logout')
+  req.logout(() => {
+    res.redirect('/users/login')
+  })
 })
 
 module.exports = router
